4ad5dcd5b6855974733f8e70e4370fb2ca0f73df
김명식 <rlaaudtlr233@naver.com>
2023-03-31 16:11:55 +0900

    [refactor] 프로젝트 -> chium.vue 파일 인라인스타일 제거 및 코드 리펙토링

--- src/components/projects/chium.vue
+++ src/components/projects/chium.vue
@@ -1,9 +1,6 @@
 <template>
   <v-card
-    style="background: white;
-      box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-      border-radius: 15px;
-      padding: 20px 20px;"
+    class="wrap-project-card"
   >
     <v-row
       justify="space-between"
@@ -13,11 +10,7 @@
         style="width: 40vw"
         cols="auto"
       >
-        <BoldText
-          class="black--text"
-          text-value="Chium - (주)느루"
-          font-size="24"
-        />
+        <h2>Chium - (주)느루</h2>
         <!-- <v-btn
           class="mt-2"
           style="color: blue;"
@@ -26,11 +19,9 @@
         >
           👉 프로젝트 상세내용 다운로드 👈
         </v-btn> -->
-        <NomalText
-          class="mt-2"
-          text-value="chium 서비스는 WebView으로 구현 되어 있으며, 폐기물 배출자와 수거업체를 연결해 주는 폐기물 안전처리 서비스 입니다. 배출자와 수거업체간의 입찰 과정을 통하여 폐기물을 처리하며 배출자는 가장 저렴한 가격에 폐기물을 배출할 수 있습니다."
-          font-size="19"
-        />
+        <p class="mt-2">
+          chium 서비스는 WebView으로 구현 되어 있으며, 폐기물 배출자와 수거업체를 연결해 주는 폐기물 안전처리 서비스 입니다. 배출자와 수거업체간의 입찰 과정을 통하여 폐기물을 처리하며 배출자는 가장 저렴한 가격에 폐기물을 배출할 수 있습니다.
+        </p>
         <div class="mt-4">
           <a
             target="blank"
@@ -52,7 +43,7 @@
               @click="googlePlayBtn"
             />
             <a
-              id="googlePlay"
+              id="chiumAos"
               target="blank"
               href="https://play.google.com/store/apps/details?id=aos.gonsoftware.chium"
               class="d-none"
@@ -66,7 +57,7 @@
               @click="appStoreBtn"
             />
             <a
-              id="appStore"
+              id="chiumIos"
               target="blank"
               href="https://apps.apple.com/kr/app/%EC%B9%98%EC%9B%80/id1623767946"
               class="d-none"
@@ -75,35 +66,21 @@
         </v-row>
         <div
           v-show="!detailToggle"
-          class="mt-4"
-          style="display: flex; cursor: pointer;"
+          class="wrap-detail-btn"
           @click="chiumDetail"
         >
-          <BoldText
-            text-value="자세히보기"
-            font-size="22"
-            style="flex: 0 0 auto;
-                width: auto;
-                max-width: 100%;"
-          />
-          <v-icon size="35">
+          <strong>자세히보기</strong>
+          <v-icon size="30">
             mdi-chevron-down
           </v-icon>
         </div>
         <div
           v-show="detailToggle"
-          class="mt-4"
-          style="display: flex; cursor: pointer;"
+          class="wrap-detail-btn"
           @click="chiumDetail"
         >
-          <BoldText
-            text-value="자세히보기"
-            font-size="22"
-            style="flex: 0 0 auto;
-                width: auto;
-                max-width: 100%;"
-          />
-          <v-icon size="35">
+          <strong>자세히보기</strong>
+          <v-icon size="30">
             mdi-chevron-up
           </v-icon>
         </div>
@@ -118,31 +95,19 @@
     </v-row>
     <v-expand-transition>
       <div v-show="detailToggle">
-        <v-divider
-          style="border: solid 1px #6D6A6A;"
-          class="my-3"
-        />
+        <hr class="my-3">
         <v-row
           class="mt-3"
           no-gutters
         >
           <v-col
-            style="width: 100px;"
             cols="auto"
-            class="pr-5"
+            class="item-type"
           >
-            <BoldText
-              class="text-end"
-              text-value="프로젝트"
-              font-size="20"
-            />
+            <strong>프로젝트</strong>
           </v-col>
           <v-col>
-            <BoldText
-              class="black--text"
-              text-value="chium"
-              font-size="20"
-            />
+            <strong class="black--text">chium</strong>
           </v-col>
         </v-row>
         <v-row
@@ -150,22 +115,13 @@
           no-gutters
         >
           <v-col
-            style="width: 100px;"
             cols="auto"
-            class="pr-5"
+            class="item-type"
           >
-            <BoldText
-              class="text-end"
-              text-value="프로젝트 기간"
-              font-size="20"
-            />
+            <strong>프로젝트 기간</strong>
           </v-col>
           <v-col>
-            <BoldText
-              class="black--text"
-              text-value="2021. 12 ~ 2022. 10, 신규 개발 및 운영"
-              font-size="20"
-            />
+            <strong class="black--text">2021. 12 ~ 2022. 10, 신규 개발 및 운영</strong>
           </v-col>
         </v-row>
         <v-row
@@ -173,49 +129,25 @@
           no-gutters
         >
           <v-col
-            style="width: 100px;"
             cols="auto"
-            class="pr-5"
+            class="item-type"
           >
-            <BoldText
-              text-value="담당 업무"
-              font-size="20"
-            />
+            <strong>담당 업무</strong>
           </v-col>
           <v-col>
-            <BoldText
-              class="black--text"
-              text-value="Front-End개발"
-              font-size="20"
-            />
-            <NomalText
-              text-value="- 상태관리 라이브러리인 Vuex를 이용하여 Props 복잡도 개선"
-              font-size="19"
-            />
-            <NomalText
-              text-value="- Jwt 토큰을 이용한 인증 로직 구현"
-              font-size="19"
-            />
-            <NomalText
-              text-value="- 아임포트 모듈을 이용하여 다날 본인인증 서비스 연동"
-              font-size="19"
-            />
-            <NomalText
-              text-value="- 재사용 되는 UI가 있지만 컴포넌트화 되지 않았던 UI들을 컴포넌트로 변경"
-              font-size="19"
-            />
-            <NomalText
-              text-value="- UI 라이브러리를 적절히 활용하여 빠른 UI 개발"
-              font-size="19"
-            />
-            <NomalText
-              text-value="- 운영 중에 발생하는 버그 및 고객 개선사항에 따른 수정"
-              font-size="19"
-            />
-            <NomalText
-              text-value="- 중복되는 Css Class를 줄이기 위해 재사용 가능한 Class 구축"
-              font-size="19"
-            />
+            <strong class="black--text">Front-End개발</strong>
+            <p>
+              - WebView를 이용한 Aos, Ios 앱 개발
+              <br>
+              Ios의 경우 앱 종료 시 웹 로컬스토리지가 초기화되는 문제가 발생하여 Native 저장소에 초기화되지 않아야 할 데이터를 저장하여 문제를 해결하였습니다.
+            </p>
+            <p>- 상태관리 라이브러리인 Vuex를 이용하여 Props 복잡도 개선</p>
+            <p>- Jwt 토큰을 이용한 인증 로직 구현</p>
+            <p>- 아임포트 모듈을 이용하여 다날 본인인증 서비스 연동</p>
+            <p>- 재사용 되는 UI가 있지만 컴포넌트화 되지 않았던 UI들을 컴포넌트로 변경</p>
+            <p>- UI 라이브러리를 적절히 활용하여 빠른 UI 개발</p>
+            <p>- 운영 중에 발생하는 버그 및 고객 개선사항에 따른 수정</p>
+            <p>- 중복되는 Css Class를 줄이기 위해 재사용 가능한 Class 구축</p>
           </v-col>
         </v-row>
 
@@ -224,21 +156,13 @@
           no-gutters
         >
           <v-col
-            style="width: 100px;"
             cols="auto"
-            class="pr-5"
+            class="item-type"
           >
-            <BoldText
-              text-value="사용 기술"
-              font-size="20"
-            />
+            <strong>사용 기술</strong>
           </v-col>
           <v-col>
-            <BoldText
-              class="black--text"
-              text-value="Vue.js, Css, Vuetify, Vuex, Git, GitHub"
-              font-size="20"
-            />
+            <strong class="black--text">Vue.js, Css, Vuetify, Vuex, Git, GitHub</strong>
           </v-col>
         </v-row>
 
@@ -247,21 +171,13 @@
           no-gutters
         >
           <v-col
-            style="width: 100px;"
             cols="auto"
-            class="pr-5"
+            class="item-type"
           >
-            <BoldText
-              text-value="개발 인원"
-              font-size="20"
-            />
+            <strong>개발 인원</strong>
           </v-col>
           <v-col>
-            <BoldText
-              class="black--text"
-              text-value="Front-End 2명, Back-End 2명 (참여율 Front-End 기준 70%)"
-              font-size="20"
-            />
+            <strong class="black--text">Front-End 2명, Back-End 2명 (참여율 Front-End 기준 70%)</strong>
           </v-col>
         </v-row>
       </div>
@@ -270,16 +186,8 @@
 </template>
 
 <script>
-import BoldText from '@/components/text/textBold.vue'
-import NomalText from '@/components/text/text.vue'
-
 export default {
   name: "Chium",
-  components: {
-    // NomalText,
-    BoldText,
-    NomalText
-  },
   data: () => ({
     detailToggle: false,
   }),
@@ -288,15 +196,45 @@ export default {
       this.detailToggle = !this.detailToggle
     },
     googlePlayBtn() {
-      document.getElementById('googlePlay').click()
+      document.getElementById('chiumAos').click()
     },
     appStoreBtn() {
-      document.getElementById('appStore').click()
+      document.getElementById('chiumIos').click()
     }
   }
 }
 </script>
 
-<style>
-
+<style scoped lang="scss">
+.wrap-project-card {
+  background: white;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 15px;
+  padding: 20px 20px;
+  .wrap-detail-btn {
+    display: flex;
+    cursor: pointer;
+    margin-top: 16px;
+  }
+  .item-type {
+    padding-right: 20px;
+    width: 100px;
+    text-align: end !important;
+  }
+  h2 {
+    font-size: 2.4rem;
+  }
+  p {
+    font-size: 1.9rem;
+    margin-bottom: 8px;
+    color: $grey;
+  }
+  a {
+    font-size: 1.9rem;
+  }
+  strong {
+    font-size: 2rem;
+    color: $grey;
+  }
+}
 </style>
